2618ce4b63b5b5e82776b547ace3cf26ba632507
PabloMorenoVera <pablomvera14@gmail.com>
2018-01-30 10:37:11 +0100

    Version 2

--- TablaMultiplicar.py
+++ TablaMultiplicar.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-#Programa para imprimir la tabla de multiplicar
+#Programa para imprimir la tabla de multiplica
+numbers = range(1,11)
+
+for n in numbers:
+    print("Tabla del ", str(n))
+    for i in numbers:
+        pass
+        print(str(n), 'por', str(i), '=', str(n*i))
